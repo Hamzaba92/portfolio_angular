2aa4969ec582f18476d12daa6023c6446fb0fb11
Hamzaba92 <hamzabaj1992@gmail.com>
2024-05-28 13:48:49 +0200

    add green & red borders included valid and invalid img's

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { LanguageService } from '../../language.service';
 import { MySkillsComponent } from '../my-skills/my-skills.component';
 @Component({
@@ -13,15 +13,49 @@ import { MySkillsComponent } from '../my-skills/my-skills.component';
 export class ContactMeComponent {
 languageService = inject(LanguageService);
 
+contactData = {
+  name: "",
+  email: "",
+  message: ""
+}
+
 checkbox: boolean = false;
 errorMsgPrivacy: boolean = false;
 isCheckboxChecked: boolean = false;
 
+borderGreenName: boolean = false;
+borderRedName: boolean = false;
+
+borderGreenMail: boolean = false;
+borderRedMail: boolean = false;
+
+borderGreenMessage: boolean = false;
+borderRedMessage: boolean = false;
+
+successImgName: boolean = true;
+errorImgName: boolean = true;
+
+successImgEmail: boolean = true;
+errorImgEmail: boolean = true;
+
+successImgMessage: boolean = true;
+errorImgMessage: boolean = true;
 
 
 
 
-  
+
+
+
+isNameValid: boolean = false;
+isEmailValid: boolean = false;
+isMessageValid: boolean = false;
+
+
+onSubmit(ngForm: NgForm){
+
+}
+
 
   acceptCheckbox(){
     if(this.checkbox){
@@ -42,6 +76,57 @@ isCheckboxChecked: boolean = false;
       }
     }
 
+
+    showHint(field: string) {
+      if (field === 'name') {
+        this.validateName();
+      } else if (field === 'email'){
+          this.validateEmail();
+        } else if(field === 'message'){
+          this.validateMessage();
+        }
+       
+      
+    }
+
+    validateName() {
+      const namePattern = /^[a-zA-Z]{3,}(?: [a-zA-Z]{2,})?$/;
+      this.isNameValid = namePattern.test(this.contactData.name);
+      this.updateStyles();
+    }
+
+    validateEmail(){
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      this.isEmailValid = emailPattern.test(this.contactData.email);
+      this.updateStyles();
+    }
+
+    validateMessage(){
+      const messagePattern = /^.{4,}$/;
+      this.isMessageValid = messagePattern.test(this.contactData.message);
+      this.updateStyles();
+    }
+
+    updateStyles() {
+      //Name
+      this.borderGreenName = this.isNameValid;
+      this.borderRedName = !this.isNameValid && this.contactData.name.length > 0;
+      this.errorImgName = !this.isNameValid && this.contactData.name.length > 0;
+      this.successImgName = this.isNameValid;
+
+      //Email
+      this.borderGreenMail = this.isEmailValid;
+      this.borderRedMail = !this.isEmailValid && this.contactData.email.length > 0;
+      this.errorImgEmail = !this.isEmailValid && this.contactData.email.length > 0;
+      this.successImgEmail = this.isEmailValid;
+
+      //Message
+      this.borderGreenMessage = this.isMessageValid;
+      this.borderRedMessage = !this.isMessageValid && this.contactData.message.length > 0;
+      this.errorImgMessage = !this.isMessageValid && this.contactData.message.length > 0;
+      this.successImgMessage = this.isMessageValid;
+
+    }
   
 
 }
